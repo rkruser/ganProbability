d13773a639a9113b4ecaa3c7bbc2c4107aa64fb8
Ryen Krusinga <krusinga@cs.umd.edu>
2018-08-01 23:57:38 -0400

    Ran mnist total experiment e11

---
+++ experiments/e11/submit2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Name: mnistTotal 
+# ID: 11
+
+#SBATCH --array=0-9
+#SBATCH --job-name=mnistTotal
+###SBATCH --qos=default
+#SBATCH --mem=16gb
+#SBATCH --account scavenger
+#SBATCH --partition scavenger
+#SBATCH --gres=gpu:1 ### gpu:p6000:1
+#SBATCH --time=12:00:00
+#SBATCH --output /fs/vulcan-scratch/krusinga/projects/ganProbability/experiments/e11/logs/out_%a.txt
+###SBATCH --error err.txt
+
+echo "Hi, I am task:"
+echo ${SLURM_ARRAY_TASK_ID}
+
+cd /fs/vulcan-scratch/krusinga/projects/ganProbability/
+module load cuda
+python run.py --pid ${SLURM_ARRAY_TASK_ID} --nprocs ${SLURM_ARRAY_TASK_COUNT} --stage 2
