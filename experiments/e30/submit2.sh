31c253fb6761d9b8d0ef8bbf0dad04940112d5e9
Ryen Krusinga <krusinga@cs.umd.edu>
2018-08-28 15:59:50 -0400

    e28 through e31

---
+++ experiments/e30/submit2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Name: birdsnapInfogan 
+# ID: 30
+
+#SBATCH --array=0-9
+#SBATCH --job-name=birdsnapInfogan
+###SBATCH --qos=default
+#SBATCH --mem=16gb
+#SBATCH --account scavenger
+#SBATCH --partition scavenger
+#SBATCH --gres=gpu:1 ### gpu:p6000:1
+#SBATCH --time=12:00:00
+#SBATCH --output /cfarhomes/krusinga/ganProb/ganProbProject/experiments/e30/logs/out_%a.txt
+###SBATCH --error err.txt
+
+echo "Hi, I am task:"
+echo ${SLURM_ARRAY_TASK_ID}
+
+cd /cfarhomes/krusinga/ganProb/ganProbProject
+module load cuda
+python run.py --pid ${SLURM_ARRAY_TASK_ID} --nprocs ${SLURM_ARRAY_TASK_COUNT} --stage 2
