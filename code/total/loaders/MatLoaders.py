f436e95e6ca8aa976c500c0c4722df671bd9b29d
Ryen Krusinga <krusinga@cs.umd.edu>
2018-09-13 11:03:22 -0400

    Basic mat loader and model should theoretically work, but must be tested

--- code/total/loaders/MatLoaders.py
+++ code/total/loaders/MatLoaders.py
@@ -1,4 +1,4 @@
-from LoaderTemplate import LoaderTemplate
+from code.total.loaders.LoaderTemplate import LoaderTemplate
 
 import torch 
 import torch.utils.data as data
@@ -8,34 +8,43 @@ from scipy.io import loadmat
 import sys
 from os.path import join
 
-# Utility function for getting indices according to proportions
-def getProportionalIndices(distribution, counts,randomize=False):
-  assert(len(distribution) == len(counts))
-  total = np.sum(counts)
-  sizes = np.array(float(total)*distribution, dtype='int')
-  if randomize:
-    ranges = [np.random.choice(counts[i],size=sizes[i],replace=True) for i in range(len(sizes))]
-  else:
-    ranges = [np.arange(s)%counts[i] for i,s in enumerate(sizes)]
-  return ranges
+
+class MNISTSize28Cols1(LoaderTemplate):
+  def __init__(self, config, args):
+    super(LoaderTemplate, self).__init__(config, args)
+    self.path = self.getPath('mnist')
+
+  def getDataset(self, outShape = None, distribution=None, labels=None, mode='train', returnClass = False):
+    return MatLoader(self.path, outShape = outShape, distribution=distribution, labels=labels, returnLabel=returnLabel, mode=mode)
 
 
-class LabeledMatLoader(data.Dataset):
-  def __init__(self, matFile, distribution=None, outShape=None, returnLabel=False, mode='train'):
+
+# Assumes you have a matfile
+# containing Xtrain, (Ytrain), Xtest, (Ytest) of the appropriate sizes
+class MatLoader(data.Dataset):
+  def __init__(self, matFile, outShape=None, distribution=None, labels=None,  returnLabel=False, mode='train'):
     self.matDirectory = matDirectory
     self.distribution = distribution
     self.outShape = outShape
     self.mode = mode
     self.returnLabel = returnLabel
 
+    self.X = None
+    self.Y = None
+
     # Load train or test
     data = loadmat(matFile)
     if mode=='test':
       self.X = data['Xtest'].astype(np.float32)
-      self.Y = data['Ytest'].astype(int).squeeze()
+      if 'Ytest' in data:
+        self.Y = data['Ytest'].astype(int).squeeze()
     else:
       self.X = data['Xtrain'].astype(np.float32)
-      self.Y = data['Ytrain'].astype(int).squeeze()
+      if 'Ytrain' in data:
+        self.Y = data['Ytrain'].astype(int).squeeze()
+
+    if returnLabel:
+      assert(self.Y is not None)
 
     # Handle input / output shapes
     self.defaultShape = self.X[0].shape #
@@ -47,12 +56,21 @@ class LabeledMatLoader(data.Dataset):
       self.reshape = False
 
     # Get labels
-    labels = np.unique(Y) #Should be sorted
-    self.classwiseSubsets = [np.where(Y==lbl) for lbl in labels]
+    if self.Y is not None:
+      if labels is not None:
+        self.labels = labels
+      else:
+        self.labels = np.unique(Y) #Should be sorted
+      self.classwiseSubsets = [np.where(Y==lbl) for lbl in self.labels]
+    else:
+      self.labels = None
+      self.classwiseSubsets = None
 
     # Resample X according to a distribution
     if distribution is not None:
-      self.counts = [len(sub) for sub in classwiseSubsets]
+      assert(self.Y is not None)
+      self.counts = np.array([len(sub) for sub in classwiseSubsets])
+      assert(np.all(self.counts>0)) #Must be true to have distribution
       self.rangeInds = getProportionalIndices(distribution, counts, randomize=False)
       self.proportionalSubsets = [cw[rangeInds[i]] for i, cw in enumerate(classwiseSubsets)]
       self.totalIndices = np.random.permutation(np.concatenate(self.proportionalSubsets))
@@ -63,7 +81,7 @@ class LabeledMatLoader(data.Dataset):
     return len(self.X)
 
   def __getitem__(self,item):
-    x = torch.from_numpy(self.X[item])
+    x = torch.from_numpy(self.X[item]) # Do I want from_numpy? Dataset could be modified
     if self.reshape:
       # Need to reshape x to have right number of colors, right scale
       if self.outShape[0] == 3 and x.size(0) == 1:
@@ -72,15 +90,16 @@ class LabeledMatLoader(data.Dataset):
         x = (x[0]*0.2989+x[1]*0.5870+x[2]*0.1140).unsqueeze(0)
 #      x = self.transform(x)
 
-    y = self.Y[item]
     if self.returnLabel:
+      y = self.Y[item]
       return x, y
     else:
       return x
 
   def sampleFromClassDistribution(self, distribution, number=1, squeeze=True):
+    assert(self.Y is not None)
     choices = np.random.choice(len(self.labels), size=number, p=distribution)
-    inds = [np.random.choice(self.classwiseSubsets[i]) for i in choices]
+    inds = np.array([np.random.choice(self.classwiseSubsets[i]) for i in choices])
     resultX = torch.Tensor(self.X[inds])
     resultY = torch.IntTensor(self.Y[inds])
     if number==1 and squeeze:
@@ -92,4 +111,14 @@ class LabeledMatLoader(data.Dataset):
       return resultX
 
     
+# Utility function for getting indices according to proportions
+def getProportionalIndices(distribution, counts,randomize=False):
+  assert(len(distribution) == len(counts))
+  total = np.sum(counts)
+  sizes = np.array(float(total)*distribution, dtype='int')
+  if randomize:
+    ranges = [np.random.choice(counts[i],size=sizes[i],replace=True) for i in range(len(sizes))]
+  else:
+    ranges = [np.arange(s)%counts[i] for i,s in enumerate(sizes)]
+  return ranges
 
--- code/total/models/DCGANModels.py
+++ code/total/models/DCGANModels.py
@@ -1,6 +1,6 @@
 from copy import copy
-from ModelTemplate import ModelTemplate, AverageMeter
-from nnModels import weights_init, NetG28, NetD28
+from code.total.models.ModelTemplate import ModelTemplate, AverageMeter
+from code.total.models.nnModels import weights_init, NetG28, NetD28, NetG64, NetD64
 
 import torch
 import torch.nn as nn
@@ -16,25 +16,63 @@ import numpy as np
 # Check for correctness
 # Fill in sampling and other functions
 
+class DCGANSize28Col3(DCGANModel):
+  def __init__(self, config, args):
+    args = copy(args)
+    args['nc'] = 3
+    args['imSize'] = 28
+    args['netGclass'] = NetG28
+    args['netDclass'] = NetD28
+    super(DCGANSize28Col3, self).__init__(config, args)
+
+class DCGANSize28Col1(DCGANModel):
+  def __init__(self, config, args):
+    args = copy(args)
+    args['nc'] = 1
+    args['imSize'] = 28
+    args['netGclass'] = NetG28
+    args['netDclass'] = NetD28
+    super(DCGANSize28Col3, self).__init__(config, args)
+
+class DCGANSize64Col1(DCGANModel):
+  def __init__(self, config, args):
+    args = copy(args)
+    args['nc'] = 1
+    args['imSize'] = 64
+    args['netGclass'] = NetG64
+    args['netDclass'] = NetD64
+    super(DCGANSize28Col3, self).__init__(config, args)
+
+class DCGANSize28Col3(DCGANModel):
+  def __init__(self, config, args):
+    args = copy(args)
+    args['nc'] = 3
+    args['imSize'] = 64
+    args['netGclass'] = NetG64
+    args['netDclass'] = NetD64
+    super(DCGANSize28Col3, self).__init__(config, args)
 
 
 # Need to do random seeding in another module
 
-class DCGAN28(ModelTemplate):
+class DCGANModel(ModelTemplate):
   def __init__(self, config, args):
-    super(DCGAN28,self).__init__(config, args)
+    super(DCGANModel,self).__init__(config, args)
     self.opt = {
       'nz': 100,
       'ngf':64,
       'ndf':64,
-      'nc':64,
+      'nc':3,
+      'imSize':28,
       'ngpu':0,
       'cuda':False,
       'lr':0.0002,
       'beta1':0.5,
+      'netGclass':NetG28,
       'netG':'',
       'netGinstance':-1,
       'netGexpNum':-1,
+      'netDclass':NetD28,
       'netD':'',
       'netDinstance':-1,
       'netDexpNum':-1,
@@ -43,21 +81,28 @@ class DCGAN28(ModelTemplate):
     args = copy(args)
     self.opt.update(args)
 
+    self.log(str(self.opt))
+
     # Perhaps there is a pythonic way to convert dict items to attributes
     self.nz = self.opt['nz']
     self.ngf = self.opt['ngf']
     self.ndf = self.opt['ndf']
     self.nc = self.opt['nc']
+    self.imSize = self.opt['imSize']
     self.lr = self.opt['lr']
     self.beta1 = self.opt['beta1']
     self.ngpu = self.opt['ngpu']
     self.cuda = self.opt['cuda']
-    self.outShape = (self.nc,28,28)
+    self.outShape = (self.nc,self.imSize,self.imSize)
 
+    self.log("outShape is "+str(self.outShape))
+
+    self.netGclass = self.opt['netGclass']
     self.netGkey = self.opt['netG']
     self.netGinstance = self.opt['netGinstance']
     self.netGexpNum = self.opt['netGexpNum']
 
+    self.netDclass = self.opt['netDclass']
     self.netDkey = self.opt['netD']
     self.netDinstance = self.opt['netDinstance']
     self.netDexpNum = self.opt['netDexpNum']
@@ -65,8 +110,8 @@ class DCGAN28(ModelTemplate):
     self.checkpointEvery = self.opt['checkpointEvery']
 
 
-    self.netG = NetG28(nz=self.nz, ngf=self.ngf, nc=self.nc, ngpu=self.ngpu)
-    self.netD = NetD28(nz=self.nz, ngf=self.ngf, nc=self.nc, ngpu=self.ngpu)
+    self.netG = self.netGclass(nz=self.nz, ngf=self.ngf, nc=self.nc, ngpu=self.ngpu)
+    self.netD = self.netDclass(nz=self.nz, ngf=self.ngf, nc=self.nc, ngpu=self.ngpu)
     self.criterion = nn.BCELoss()
     self.optimizerG = optim.Adam(self.netG.parameters(), lr=self.lr, betas=(self.beta1,0.999))
     self.optimizerD = optim.Adam(self.netD.parameters(), lr=self.lr, betas=(self.beta1,0.999))
@@ -89,7 +134,6 @@ class DCGAN28(ModelTemplate):
       self.netD.cuda()
       self.criterion.cuda()
 
-
   # data tracking
     self.images = []
     self.errG = []
@@ -97,6 +141,11 @@ class DCGAN28(ModelTemplate):
 
 
   def train(self, loaderTemplate, nepochs):
+    # Reset for this run
+    self.images = []
+    self.errG = []
+    self.errD = []
+
     dataloader = loaderTemplate.getDataloader(outShape=self.outShape, mode='train', returnClass=False)
 
     for epoch in range(nepochs):
@@ -162,16 +211,17 @@ class DCGAN28(ModelTemplate):
       if (epoch+1)%self.checkpointEvery == 0:
         self.save(checkpointNum = epoch)
 
+    self.save()
       
   # Sample 
   def sample(self, nSamples):
     pass
 
   # method should be one of 'numerical', 'exact'
-  def probSample(self, nSamples, deepFeatures=None, method='numerical'):
+  def probSample(self, nSamples, deepFeatures=None, method='numerical', epsilon=1e-5):
     codes = torch.FloatTensor(nSamples,self.nz).normal_(0,1)
-    results = self.getProbs(codes, method=method)
-    results['codes'] = codes
+    results = self.getProbs(codes, method=method, epsilon=epsilon)
+    results['codes'] = codes.cpu().numpy()
     # if deepFeatures is not None:
     #   Run the deep feature network on all images and put in results
     return results
@@ -179,33 +229,32 @@ class DCGAN28(ModelTemplate):
   # Move this to a library function later
   # Get the probabilities of a set of codes
   def getProbs(self, codes, method='numerical', epsilon=1e-5):
-    assert(len(codes.size()) == 4)
-
     if method=='numerical':
-      noise = torch.FloatTensor(2*self.nz+1,nz,1,1)
+      noise = torch.FloatTensor(2*self.nz+1,self.nz,1,1)
     else:
       noise = torch.FloatTensor(1, self.nz, 1, 1) 
     b = torch.eye(self.nz)*epsilon # Add/subtract to code
 
     if self.cuda:
       noise.cuda()
-      codes = codes.cuda() #copy them?
-      b.cuda()
+#      b.cuda() # not necessary?
 
     gauss_const = -self.nz*np.log(np.sqrt(2*np.pi))
     log_const = 1.0
 
-    nsamples = codes.size(0)
-    images = np.empty([nsamples, self.nc, 28, 28])
-    probs = np.empty([nsamples])
-    jacob = np.empty([nsamples, self.nz])
-    nX = self.nc*28*28
+    nSamples = codes.size(0)
+    images = np.empty([nSamples, self.nc, self.imSize, self.imSize])
+    probs = np.empty([nSamples])
+    jacob = np.empty([nSamples, self.nz])
+    nX = self.nc*self.imSize*self.imSize
 
     self.netG.eval()
     for i in range(codes.size(0)):
+      self.log("Probability sampling {0}".format(i))
+
       J = np.empty([nX, self.nz])
 
-      a = codes[0]
+      a = codes[0].view(1,-1)
       if method=='numerical':
         noise.copy_(torch.cat((a,a+b,a-b),0).unsqueeze(2).unsqueeze(3))
         noisev = Variable(noise, volatile=True) #volatile helps with memory?
@@ -224,17 +273,17 @@ class DCGAN28(ModelTemplate):
           J[k] = noisev.grad.data.cpu().numpy().squeeze()
         I = fake.data.cpu().numpy()
 
-      images[i] = I[0,:].reshape(self.nc, 28, 28)
+      images[i] = I[0,:].reshape(self.nc, self.imSize, self.imSize)
       
       R = np.linalg.qr(J, mode='r')
       dummy = R.diagonal().copy()
       jacob[i] = dummy.copy() # No modification yet
       dummy[np.where(np.abs(dummy) < 1e-20)] = 1
-      prob[i] = -log_const*0.5*np.sum(Z**2)+gauss_const - np.log(np.abs(dummy)).sum()
+      probs[i] = -log_const*0.5*np.sum(Z**2)+gauss_const - np.log(np.abs(dummy)).sum()
 
     
     allData = {'images': images.astype(np.float32),
-               'probs': prob.astype(np.float32),
+               'prob': probs.astype(np.float32), #no s to be consistent
                'jacob': jacob.astype(np.float32)}
     return allData
 
@@ -250,9 +299,13 @@ class DCGAN28(ModelTemplate):
 
 
 
-  def saveCheckpoint(self, checkpointNum):
-    self.save(self.netG.state_dict(), 'netG', instance=checkpointNum, saver='torch')
-    self.save(self.netD.state_dict(), 'netD', instance=checkpointNum, saver='torch')
+  def saveCheckpoint(self, checkpointNum=None):
+    if checkpointNum is not None:
+      self.save(self.netG.state_dict(), 'netG', instance=checkpointNum, saver='torch')
+      self.save(self.netD.state_dict(), 'netD', instance=checkpointNum, saver='torch')
+    else:
+      self.save(self.netG.state_dict(), 'netG', saver='torch')
+      self.save(self.netD.state_dict(), 'netD', saver='torch')
 
 
   def getAnalysisData(self):
