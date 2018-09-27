17895c6c83282aa0b154ed871ae4df459b0c99bb
Ryen Krusinga <krusinga@cs.umd.edu>
2018-09-27 09:22:33 -0400

    Ran experiments 41 and 42

---
+++ experiments/e42/submit.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Name: stage1 
+# ID: 42
+
+###SBATCH --array=0-9 #Investigate
+#SBATCH --job-name=stage1
+###SBATCH --qos=default
+#SBATCH --account scavenger
+#SBATCH --partition scavenger
+#SBATCH --mem=16gb
+###SBATCH --partition dpart
+#SBATCH --gres=gpu:1
+#SBATCH --time=12:00:00
+#SBATCH --output /fs/vulcan-scratch/krusinga/projects/ganProbability/experiments/e42/logs/out.out
+###SBATCH --error err.txt
+
+echo "Hello:"
+
+cd /fs/vulcan-scratch/krusinga/projects/ganProbability/
+module load cuda
+python run.py --stage 1 --id 42 --masterconfig /fs/vulcan-scratch/krusinga/projects/ganProbability/master.yaml
--- runPrepro.sh
+++ runPrepro.sh
@@ -5,4 +5,4 @@
 #SBATCH --time=12:00:00
 #SBATCH --job-name=birdsnapPrepro
 cd /fs/vulcan-scratch/krusinga/projects/ganProbability
-python preprocessData.py --name cub_200_2011_ --folder /vulcan/scratch/krusinga/CUB_200_2011/images --outSize 32
+python preprocessData.py --name mnist --folder /vulcan/scratch/krusinga/mnist --outSize 128
