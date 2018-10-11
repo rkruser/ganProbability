94c3b1d4793393438cd68538271f0a432655c09c
Ryen Krusinga <krusinga@cs.umd.edu>
2018-10-11 15:04:08 -0400

    Ran e66 and e67

--- figures.py
+++ figures.py
@@ -38,7 +38,7 @@ def extractProbabilityStats(probs, ims, labels, take=36):#, plot=False, save=Tru
         imAccs.append([lbl, sortedIms[mask][:take], sortedIms[mask][-take:]])
     return labelAccs, imAccs, sortedProbs
 
-def plotHistograms(accs, suffix='',sortedProbs=None):
+def plotHistograms(accs, path='', suffix='',sortedProbs=None):
     for d in accs:
         lbl, cumul, probs = d[0], d[1], d[2]
         plt.plot(range(len(cumul)), cumul, label='Label {}'.format(lbl))
@@ -46,12 +46,12 @@ def plotHistograms(accs, suffix='',sortedProbs=None):
     plt.title("Class accumulations")
     plt.xlabel("Sorted point index")
     plt.ylabel("Number of preceding points with class label")
-    plt.savefig('./experiments/e65/analysis/cumulativeClassHistogram_{}.png'.format(suffix))
+    plt.savefig(osp.join(path,'cumulativeClassHistogram_{}.png'.format(suffix)))
     plt.close()
 
     if sortedProbs is not None:
         plt.hist(sortedProbs, bins=100)
-        plt.savefig('./experiments/e65/analysis/totalProbHistogram_{}.png'.format(suffix))
+        plt.savefig(osp.join(path,'totalProbHistogram_{}.png'.format(suffix)))
         plt.close()
 
     for d in accs:
@@ -60,16 +60,16 @@ def plotHistograms(accs, suffix='',sortedProbs=None):
         plt.title("Class {} logprob histogram".format(lbl))
         plt.xlabel("Log probability")
         plt.ylabel("Number of points")
-        plt.savefig('./experiments/e65/analysis/probHistClass_{0}_{1}.png'.format(suffix,lbl))
+        plt.savefig(osp.join(path,'probHistClass_{0}_{1}.png'.format(suffix,lbl)))
         plt.close()
 
-def plotTopBottom(imAccs, suffix=''):
+def plotTopBottom(imAccs, path='', suffix=''):
     for imList in imAccs:
         lbl, bottom, top = imList[0], imList[1], imList[2]
         bottom = np.transpose(bottom,(0,2,3,1))*0.5+0.5
         top = np.transpose(top,(0,2,3,1))*0.5+0.5
-        imageArray({'images':bottom},'./experiments/e65/analysis/bottom_{0}_{1}.png'.format(suffix,str(lbl)))
-        imageArray({'images':top},'./experiments/e65/analysis/top_{0}_{1}.png'.format(suffix,str(lbl)))
+        imageArray({'images':bottom},osp.join(path,'bottom_{0}_{1}.png'.format(suffix,str(lbl))))
+        imageArray({'images':top},osp.join(path,'top_{0}_{1}.png'.format(suffix,str(lbl))))
         plt.close()
  
 
@@ -137,16 +137,79 @@ def mnistNumerical():
     pixelStats = extractProbabilityStats(probsPixel, Xtest, Ytest)
     deepStats = extractProbabilityStats(probsDeep, Xtest, Ytest)
     
-    plotHistograms(pixelStats[0],suffix='pixel', sortedProbs = pixelStats[2])
-    plotHistograms(deepStats[0], suffix='deep', sortedProbs = deepStats[2])
-#    plotTopBottom(pixelStats[1], suffix='pixel')
-#    plotTopBottom(deepStats[1], suffix='deep')
+    plotHistograms(pixelStats[0],path = './experiments/e65/analysis/', suffix='pixel', sortedProbs = pixelStats[2])
+    plotHistograms(deepStats[0], path = './experiments/e65/analysis/', suffix='deep', sortedProbs = deepStats[2])
+#    plotTopBottom(pixelStats[1], path='./experiments/e65/analysis/', suffix='pixel')
+#    plotTopBottom(deepStats[1], path='./experiments/e65/analysis/', suffix='deep')
     
+def cifar10Numerical():
+    #prefix = '/fs/vulcan-scratch/krusinga/projects/ganProbability/generated/canonical/DCGAN/MNIST/size32_3'
+    masterpath = '/fs/vulcan-scratch/krusinga/projects/ganProbability/master.yaml'
+    cuda = True
+    currentExperiment = 65
+    files = FileManager(masterpath, currentExperiment = currentExperiment)
+
+    cifar32loader = Modules['CFAR10Size32Cols3']({'name':'CIFAR10', 'fileHandler':files, 'dependencies':[]},{})
+    cifar32 = cifar32loader.getDataset(outShape = (3,32,32), distribution=None, labels=None, mode='test', returnLabel = True)
+    #Xtest = np.concatenate([cifar32.X, cifar32.X, cifar32.X], axis=1)
+    Xtest = cifar32.X
+    Ytest = cifar32.Y
+    print Xtest.shape
+    print Ytest.shape
+
+    probsPixel = None
+    probsDeep = None
+    if not osp.exists('./generated/e67/data/probsPixel.pickle'):
+        numericalPixelRegressor = Modules['RegressorSize32Col3']({'name':'NumericalPixels',
+                                                                  'fileHandler':files,
+                                                                  'dependencies':[]},
+                                                                 {'netPkey':'reg_cifar10_32_3_numerical_pixel','cuda':cuda})
+        numericalDeepRegressor = Modules['FeatRegressor10']({'name':'NumericalDeep',
+                                                                  'fileHandler':files,
+                                                                  'dependencies':[]},
+                                                                 {'netPkey':'reg_cifar10_32_3_numerical_deep','cuda':cuda})
+        lenetModule = Modules['LenetSize32Cols3']({'name':'lenetCifar32',
+                                                'fileHandler':files,
+                                                'dependencies':[]},
+                                                {'lenetKey':'deep_cifar10_32_3','cuda':cuda})
+        lenet = NthArgWrapper(lenetModule.getModel(), 1)
+        
+        XtestRun = Variable(torch.Tensor(Xtest))
+        if cuda:
+            XtestRun = XtestRun.cuda()
+        XtestEmbedded = lenet(XtestRun)
+        print "Getting pixel probs"
+        probsPixel = numericalPixelRegressor.netP(XtestRun).data.cpu().numpy()
+        print "Shape:",probsPixel.shape
+        print "Getting deep probs"
+        probsDeep = numericalDeepRegressor.netP(XtestEmbedded).data.cpu().numpy()
+        print "Shape:",probsDeep.shape
+
+
+        print "Dumping pixel"
+        pickle.dump(probsPixel,open('./generated/e67/data/probsPixel.pickle','w')) 
+        print "Dumping deep"
+        pickle.dump(probsDeep,open('./generated/e67/data/probsDeep.pickle','w')) 
+
+    else:
+        probsPixel = pickle.load(open('./generated/e67/data/probsPixel.pickle','r')) 
+        probsDeep = pickle.load(open('./generated/e67/data/probsDeep.pickle','r')) 
+       
+
+    
+    pixelStats = extractProbabilityStats(probsPixel, Xtest, Ytest)
+    deepStats = extractProbabilityStats(probsDeep, Xtest, Ytest)
+    
+    plotHistograms(pixelStats[0],path='./experiments/e67/analysis', suffix='pixel', sortedProbs = pixelStats[2])
+    plotHistograms(deepStats[0], path='./experiments/e67/analysis', suffix='deep', sortedProbs = deepStats[2])
+    plotTopBottom(pixelStats[1], path='./experiments/e67/analysis/', suffix='pixel')
+    plotTopBottom(deepStats[1], path='./experiments/e67/analysis/', suffix='deep')
 
 
 
 if __name__=='__main__':
-    mnistNumerical()
+#    mnistNumerical()
+    cifar10Numerical()
 
 
 
